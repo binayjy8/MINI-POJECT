d27c3837f795e69178d9da4d969a12f90bacca8d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-05 07:43:02 +0530

    code push

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ let inp = document.querySelector("input");
 btn.addEventListener("click", function () {
     let item = document.createElement("li");
     item.innerText = inp.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
     ul.appendChild(item);
     inp.value = "";
 });
